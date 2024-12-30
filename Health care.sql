fc4d1c39ed82138f05d7a41ef42f9fe7f1c4951f
Niranjan2121k <niranjankalaiselvan@gmail.com>
2024-12-30 16:44:50 +0530

    Update Health care.sql

--- Health care.sql
+++ Health care.sql
@@ -1,6 +1,8 @@
-use portfolio_project;
+Portfolio Project SQL Queries
 
--- 1.Finding the Deatils Of the Doctor who Treated 'Diabetes'--
+USE portfolio_project;
+
+-- 1. Finding the Details of the Doctor who Treated 'Diabetes' --
 SELECT 
     d.Doctor_ID,
     d.d_Worker_ID,
@@ -16,11 +18,9 @@ WHERE
     d.d_Worker_id = w.Worker_id
         AND d.doctor_id = di.doctor_id
         AND illness = 'diabetes';
--- ANS=  51235, 123456, Tilda, White, Diabetes, 193258 --
+-- ANS: 51235, 123456, Tilda, White, Diabetes, 193258 --
 
-
-
--- 2.Details Of Patients Who have ben prescribed'B205' --
+-- 2. Details of Patients Who Have Been Prescribed 'B205' --
 SELECT 
     p.Patient_ID,
     fname,
@@ -41,9 +41,9 @@ WHERE
     p.patient_id = mp.Patient_id
         AND m.medication_id = 'B205'
         AND mp.medication_id = m.medication_id;
--- ANS= 975913, Harry, Sax, (643)764-1256, M, 21, O-, 102, B205, 5, 2026-09-20 --
+-- ANS: 975913, Harry, Sax, (643)764-1256, M, 21, O-, 102, B205, 5, 2026-09-20 --
 
--- 3.TOTAL NUMBER OF DOCTORS IN EACH DEPARTMENT --
+-- 3. Total Number of Doctors in Each Department --
 SELECT 
     d.Department_ID, COUNT(doctor_id)
 FROM
@@ -52,9 +52,9 @@ FROM
 WHERE
     d.department_id = doc.department_id
 GROUP BY 1;
--- Ans 1,2,1,1,1 --
+-- Ans: 1, 2, 1, 1, 1 --
 
--- 4.NAMES AND PH NO OF ALL PATIENTS WITH 'DIABETES' --
+-- 4. Names and Phone Numbers of All Patients with 'Diabetes' --
 SELECT 
     fname, lname, telephone, illness
 FROM
@@ -63,11 +63,11 @@ FROM
 WHERE
     p.patient_id = d.patient_id
         AND illness = 'diabetes';
--- Jenny, Tayla, (642)176-7421, Diabetes --
+-- ANS: Jenny, Tayla, (642)176-7421, Diabetes --
 
--- 5.NAMES AND ID OF DOCTORS WHO WORK IN THE ER DEPT --
+-- 5. Names and IDs of Doctors Who Work in the ER Department --
 SELECT 
-    Doctor_ID, CONCAT(fname, ' ', lname)
+    Doctor_ID, CONCAT(fname, ' ', lname) AS Full_Name
 FROM
     doctor d,
     worker w
@@ -75,13 +75,13 @@ WHERE
     d.D_Worker_ID = w.Worker_ID
         AND department_id = 'er';
 
--- 6.Total salary expenditure --
+-- 6. Total Salary Expenditure --
 SELECT 
-    SUM(salary)
+    SUM(salary) AS Total_Salary_Expenditure
 FROM
     worker;
 
--- 7.CAFETERIA STAFF WITH JOB POSITION AND FOOD TYPE --
+-- 7. Cafeteria Staff with Job Position and Food Type --
 SELECT 
     s.Staff_ID, s.job_title, Position, Food_Type
 FROM
@@ -92,9 +92,9 @@ WHERE
     c.cafeteria_id = cs.cafeteria_id
         AND s.staff_id = cs.staff_id;
 
--- 8.Avg age of patients diagnosed with the 'flu' --
+-- 8. Average Age of Patients Diagnosed with the 'Flu' --
 SELECT 
-    *, AVG(age)
+    *, AVG(age) AS Average_Age
 FROM
     diagnosis d,
     patient p
